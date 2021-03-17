c4a6a4c36e526828486ee97a32ea873c83aaa43c
Crystinum <mattlee525@gmail.com>
2021-03-17 00:16:58 -0700

    Finished Forward Selection

--- FeatureSelectionNN/source/main.cpp
+++ FeatureSelectionNN/source/main.cpp
@@ -16,13 +16,18 @@ using namespace std;
 
 int main() {
 	
-	string fname = "CS170_SMALLtestdata__56.txt";
+	string fname = "CS170_largetestdata__41.txt";
 
 	vector<vector<double>> df = parse(fname);
 
 	feature_search_demo(df);
 	
-	/*cout << fixed << setprecision(8);
+	cout << fixed << setprecision(8);
+
+	unordered_set<int> curr_set;
+	curr_set.insert(1);
+
+	/*df.erase(df.begin());
 
 	for (int i = 0; i < df.size(); i++) {
 		for (int j = 0; j < df.at(i).size(); j++) {
@@ -31,8 +36,25 @@ int main() {
 			cout << df[i][j] << "    ";
 		}
 		cout << endl;
+	}
+	
+	vector<vector<double>> asdf = { {1, 2, 3, 4, 5, 6, 7, 8, 9},{1, 2, 3, 4, 5, 6, 7, 8, 9} ,{1, 2, 3, 4, 5, 6, 7, 8, 9} };
+	cout << "old ----------------" << endl;
+	for (int i = 0; i < asdf.size(); i++) {
+		for (int j = 0; j < asdf[0].size(); j++) {
+			cout << asdf[i][j] << "   ";
+		}
+		cout << endl;
+	}
+	
+	asdf.erase(asdf.begin());
+	cout << "newwwww ------------" << endl;
+	for (int i = 0; i < asdf.size(); i++) {
+		for (int j = 0; j < asdf[0].size(); j++) {
+			cout << asdf[i][j] << "   ";
+		}
+		cout << endl;
 	}*/
-
 
 	cout << "program finished" << endl;
 }
